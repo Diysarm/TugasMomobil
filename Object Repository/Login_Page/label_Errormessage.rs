029a8f7de96ed9160d47502b1d5c100953b95ee4
muhra <muhra@MRasyid>
2023-12-15 11:36:42 +0700

    Login logout regis v3

---
+++ Object Repository/Login_Page/label_Errormessage.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_ErrorMessage</name>
+   <tag></tag>
+   <elementGuidId>ac731a3c-200f-4e3f-ab50-d0cd8252529d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[@id='label_alert_Invalid Username/Password.']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
--- Test Suites/Negative_Login.ts
+++ Test Suites/Negative_Login.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Login_Logout</name>
+   <name>Negative_Login</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,19 +10,12 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>b37fab53-7f58-4bb7-ac81-99b13f362968</testSuiteGuid>
+   <testSuiteGuid>0cd54965-0cbd-4ec9-b270-bd46d74902c8</testSuiteGuid>
    <testCaseLink>
-      <guid>cb6be3aa-7308-46ea-b8b0-2bb962f43c1f</guid>
+      <guid>f80bd368-2bec-4f95-ac73-d79c0b62c7c4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Login_Logout_Registrasi/Login</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>4a2e950a-7cb2-4e30-9705-136fd2066da3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Login_Logout_Registrasi/Logout</testCaseId>
+      <testCaseId>Test Cases/Login_Logout_Registrasi/Negative_Login</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
